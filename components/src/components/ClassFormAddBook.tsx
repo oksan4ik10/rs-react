2bea8f5bdf70f6f0061c95e77b7c7dda07ada068
Kalinina Oksana <ksan4ik92@gmail.com>
2023-03-23 11:45:55 +0300

    feat: add files in form

--- components/src/components/ClassFormAddBook.tsx
+++ components/src/components/ClassFormAddBook.tsx
@@ -1,44 +1,115 @@
 import React, { FormEvent, RefObject } from 'react';
-import { IOneBook } from '../types/types';
-import * as objBooks from '../date.json';
+import { ClassOptions } from './ClassOptions';
+import { IMyStatelikes } from '../types/types';
+
 export class ClassFormAddBook extends React.Component {
   title: RefObject<HTMLInputElement>;
   year: RefObject<HTMLInputElement>;
   selectRef: RefObject<HTMLSelectElement>;
-  books: IOneBook[];
+  radioRefHorror: RefObject<HTMLInputElement>;
+  radioRefFantasy: RefObject<HTMLInputElement>;
+  radioRefDetective: RefObject<HTMLInputElement>;
+  radioRefOther: RefObject<HTMLInputElement>;
+  check: RefObject<HTMLInputElement>;
+  fileInput: RefObject<HTMLInputElement>;
+  images: string[];
+  state: IMyStatelikes;
   constructor(props: Record<string, never>) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.title = React.createRef();
     this.year = React.createRef();
     this.selectRef = React.createRef();
-    this.books = objBooks.books;
+    this.radioRefHorror = React.createRef();
+    this.radioRefFantasy = React.createRef();
+    this.radioRefDetective = React.createRef();
+    this.radioRefOther = React.createRef();
+    this.check = React.createRef();
+    this.fileInput = React.createRef();
+    this.images = [];
+
+    this.state = {
+      date: '',
+      count: 10,
+    };
   }
 
   handleSubmit(event: FormEvent) {
-    if (this.selectRef.current) alert('A name was submitted: ' + this.selectRef.current.value);
     event.preventDefault();
-    const author = new Set(this.books.map((item) => item.author));
+    console.log();
+    const radio = this.radioRefDetective.current?.checked
+      ? this.radioRefDetective.current.value
+      : this.radioRefFantasy.current?.checked
+      ? this.radioRefFantasy.current.value
+      : this.radioRefHorror.current?.checked
+      ? this.radioRefHorror.current.value
+      : this.radioRefOther.current?.value;
+    console.log(radio);
+    const check = this.check.current?.checked;
+    if (this.fileInput.current) {
+      if (this.fileInput.current.files) {
+        const selecteds = this.fileInput.current.files[0];
+        this.images.push(URL.createObjectURL(selecteds));
+        this.setState({ date: this.images[0] });
+      }
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Title:
-          <input type="text" ref={this.title} />
-        </label>
-        <label>
-          Year:
-          <input type="date" ref={this.year} />
-        </label>
-        <span>Author</span>
-        <select ref={this.selectRef}>
-          <option value="J. R. R. Tolkien">J. R. R. Tolkien</option>
-          <option value="Arthur Conan Doyle">Arthur Conan Doyle</option>
-        </select>
-        <input type="submit" value="Create card" />
-      </form>
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title:
+            <input type="text" ref={this.title} />
+          </label>
+          <label>
+            Year:
+            <input type="date" ref={this.year} />
+          </label>
+          <span>Author</span>
+          <select ref={this.selectRef}>
+            <ClassOptions />
+          </select>
+          <h4>Genre</h4>
+          <p>
+            <label>
+              Horror
+              <input type="radio" ref={this.radioRefHorror} name="radio" value="Horror" />
+            </label>
+            <label>
+              Fantasy
+              <input type="radio" ref={this.radioRefFantasy} name="radio" value="Fantasy" />
+            </label>
+            <label>
+              Detective
+              <input type="radio" ref={this.radioRefDetective} name="radio" value="Detective" />
+            </label>
+            <label>
+              Other
+              <input
+                defaultChecked
+                type="radio"
+                ref={this.radioRefOther}
+                name="radio"
+                value="Other"
+              />
+            </label>
+          </p>
+
+          <p>
+            Like
+            <input type="checkbox" ref={this.check} />
+          </p>
+          <label>
+            Upload file:
+            <input type="file" ref={this.fileInput} accept="image/*" />
+          </label>
+
+          <input type="submit" value="Create card" />
+        </form>
+        <img src={this.state.date} style={{ width: 300 }} />
+      </>
     );
   }
 }
---
+++ components/src/components/ClassOptions.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as objBooks from '../date.json';
+export class ClassOptions extends React.Component {
+  books: string[];
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.books = Array.from(new Set(objBooks.books.map((item) => item.author)));
+  }
+  render(): React.ReactNode {
+    return (
+      <>
+        {this.books.map((item, index) => (
+          <option key={index} value={item}>
+            {item}
+          </option>
+        ))}
+      </>
+    );
+  }
+}
